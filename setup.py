7e9fc5369557270905c58166c82bcc38cba06cbd
Jesse Wei <jessew13@email.unc.edu>
2023-03-27 10:53:48 -0400

    Attempt to fix unit tests

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 from setuptools import setup
 
-APP = ["gui.py"]
-OPTIONS = {"argv_emulation": True}
-
-setup(app=APP, options={"py2app": OPTIONS}, setup_requires=["py2app"])
-
 if __name__ == "__main__":
     setup()
