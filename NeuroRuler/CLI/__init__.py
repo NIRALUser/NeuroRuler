c645c0a546e440313bd3f0dc07f6133f79764cbd
Eric Schneider <u1643364@live.unc.edu>
2023-04-21 21:18:49 -0400

    ...And Just for All

--- NeuroRuler/CLI/__init__.py
+++ NeuroRuler/CLI/__init__.py
@@ -4,9 +4,12 @@ from NeuroRuler.CLI import {function}
 
 where NeuroRuler.CLI is the name of the package this __init__.py file is in."""
 
+import NeuroRuler.CLI.main as main
+import NeuroRuler.utils.parser as parser
+
 
 def cli() -> None:
     """Run CLI."""
-    parser.parse_gui_config()
+    parser.parse_cli_config()
     parser.parse_cli()
-    climain.main()
+    main.main()
--- cli.py
+++ cli.py
@@ -6,6 +6,6 @@ Run with ./cli.py. If that doesn't work, run however you normally run a Python s
 
 # Imports from NeuroRuler/CLI/__init__.py
 # This is exactly what the user can access after installing via pip
-from NeuroRuler import cli
+from NeuroRuler.CLI import cli
 
 cli()
